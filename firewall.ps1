6119e9840350c987c94d36cac38a6f05bf41174b
Korzhov Mihail <67170016+kiborg85@users.noreply.github.com>
2021-08-20 19:43:09 +0300

    Add files via upload

---
+++ firewall.ps1
@@ -0,0 +1,7 @@
+﻿#get-eventlog -LogName Security -after ((Get-date).AddDays(-1)) | Where-Object {$_.InstanceID -eq "4624" -and $_.ReplacementStrings[8] -eq "10"} |`
+# select-object {$_.TimeGenerated;$_.ReplacementStrings[5];$_.ReplacementStrings[18]}
+ $ip = get-eventlog -LogName Security -after ((get-date).AddMinutes(-2)) | Where-Object {$_.InstanceID -eq "4624" -and $_.ReplacementStrings[8] -eq "10"} | `
+ select-object {$_.ReplacementStrings[18]} | select-object -first 1
+ $ipadr = $ip.'$_.ReplacementStrings[18]'
+ Write-Host $ipadr
+echo y | &("C:\Windows\System32\plink.exe") -pw "ipa" api@10.20.36.1 "/ip firewall address-list add address=$ipadr list=RDP-white timeout=3000 comment=$env:COMPUTERNAME"
