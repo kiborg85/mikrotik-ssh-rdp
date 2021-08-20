43255ad12d7b07bc6779fd9304e7ae9239ccc689
Korzhov Mihail <67170016+kiborg85@users.noreply.github.com>
2021-08-20 19:53:11 +0300

    Update firewall.ps1
    auto detect GW

--- firewall.ps1
+++ firewall.ps1
@@ -1,7 +1,7 @@
-﻿#get-eventlog -LogName Security -after ((Get-date).AddDays(-1)) | Where-Object {$_.InstanceID -eq "4624" -and $_.ReplacementStrings[8] -eq "10"} |`
-# select-object {$_.TimeGenerated;$_.ReplacementStrings[5];$_.ReplacementStrings[18]}
- $ip = get-eventlog -LogName Security -after ((get-date).AddMinutes(-2)) | Where-Object {$_.InstanceID -eq "4624" -and $_.ReplacementStrings[8] -eq "10"} | `
- select-object {$_.ReplacementStrings[18]} | select-object -first 1
+$login=api
+$pass=ipa
+ $ip = get-eventlog -LogName Security -after ((get-date).AddMinutes(-2)) | Where-Object {$_.InstanceID -eq "4624" -and $_.ReplacementStrings[8] -eq "10"} | select-object {$_.ReplacementStrings[18]} | select-object -first 1
  $ipadr = $ip.'$_.ReplacementStrings[18]'
  Write-Host $ipadr
-echo y | &("C:\Windows\System32\plink.exe") -pw "ipa" api@10.20.36.1 "/ip firewall address-list add address=$ipadr list=RDP-white timeout=3000 comment=$env:COMPUTERNAME"
+ $gw=(Get-WmiObject -Class Win32_IP4RouteTable | where { $_.destination -eq '0.0.0.0' -and $_.mask -eq '0.0.0.0'} | Sort-Object metric1 | select nexthop).nexthop
+echo y | &("C:\Windows\System32\plink.exe") -pw $pass $login$gw "/ip firewall address-list add address=$ipadr list=RDP-white timeout=3000 comment=$env:COMPUTERNAME"
