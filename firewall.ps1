925cf25246e8ea849eb0828acfa18877ef46046c
Korzhov Mihail <67170016+kiborg85@users.noreply.github.com>
2021-08-23 19:36:26 +0300

    Update firewall.ps1
    исправил не верный фильтр поиска айпишника корректного клиента. По прошлому фильтру

--- firewall.ps1
+++ firewall.ps1
@@ -1,6 +1,6 @@
 $login=api
 $pass=ipa
- $ip = get-eventlog -LogName Security -after ((get-date).AddMinutes(-2)) | Where-Object {$_.InstanceID -eq "4624" -and $_.ReplacementStrings[8] -eq "10"} | select-object {$_.ReplacementStrings[18]} | select-object -first 1
+ $ip = get-eventlog -LogName Security -after ((get-date).AddMinutes(-2)) | Where-Object {$_.InstanceID -eq "4624" -and $_.ReplacementStrings[18] -ne "-"} | select-object {$_.ReplacementStrings[18]} | select-object -first 1
  $ipadr = $ip.'$_.ReplacementStrings[18]'
  Write-Host $ipadr
  $gw=(Get-WmiObject -Class Win32_IP4RouteTable | where { $_.destination -eq '0.0.0.0' -and $_.mask -eq '0.0.0.0'} | Sort-Object metric1 | select nexthop).nexthop
