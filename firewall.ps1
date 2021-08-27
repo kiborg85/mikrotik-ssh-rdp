827843c243efec667d676d0941105ade85578275
Korzhov Mihail <67170016+kiborg85@users.noreply.github.com>
2021-08-27 18:12:27 +0300

    Update firewall.ps1

--- firewall.ps1
+++ firewall.ps1
@@ -1,7 +1,7 @@
-$login=api
-$pass=ipa
+$login="api"
+$pass="ipa"
  $ip = get-eventlog -LogName Security -after ((get-date).AddMinutes(-2)) | Where-Object {$_.InstanceID -eq "4624" -and $_.ReplacementStrings[18] -ne "-"} | select-object {$_.ReplacementStrings[18]} | select-object -first 1
  $ipadr = $ip.'$_.ReplacementStrings[18]'
  Write-Host $ipadr
  $gw=(Get-WmiObject -Class Win32_IP4RouteTable | where { $_.destination -eq '0.0.0.0' -and $_.mask -eq '0.0.0.0'} | Sort-Object metric1 | select nexthop).nexthop
-echo y | &("C:\Windows\System32\plink.exe") -pw $pass $login$gw "/ip firewall address-list add address=$ipadr list=RDP-white timeout=3000 comment=$env:COMPUTERNAME"
+echo y | &("C:\Windows\System32\plink.exe") -pw $pass $login@$gw "/ip firewall address-list add address=$ipadr list=RDP-white timeout=86000 comment=$env:COMPUTERNAME"
